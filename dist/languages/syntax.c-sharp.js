4c4c88cbf895ae4d27f53e4e94bed5a684d63b4f
William Troup <williamtroup@gmail.com>
2023-12-09 14:51:33 +0000

    Updated to v1.7.1.

--- dist/languages/syntax.c-sharp.js
+++ dist/languages/syntax.c-sharp.js
@@ -1,4 +1,4 @@
-/*! Syntax.js v1.7.0 | (c) Bunoon | MIT License */
+/*! Syntax.js v1.7.1 | (c) Bunoon | MIT License */
 $syntax.addLanguage( "c-sharp", {
     friendlyName: "C#",
     keywords: [
--- dist/languages/syntax.typescript.js
+++ dist/languages/syntax.typescript.js
@@ -1,4 +1,4 @@
-/*! Syntax.js v1.7.0 | (c) Bunoon | MIT License */
+/*! Syntax.js v1.7.1 | (c) Bunoon | MIT License */
 $syntax.addLanguage( "typescript", {
     friendlyName: "TypeScript",
     keywords: [
