5238416bef2eed06642ce270a2ec6b3fcff5e2ad
William Troup <williamtroup@gmail.com>
2024-07-09 22:38:46 +0100

    Added enum.ts.

---
+++ src/ts/enum.ts
@@ -0,0 +1,23 @@
+/**
+ * Syntax.js
+ * 
+ * A lightweight, and easy-to-use, JavaScript library for code syntax highlighting!
+ * 
+ * @file        enum.js
+ * @version     v3.0.0
+ * @author      Bunoon
+ * @license     MIT License
+ * @copyright   Bunoon 2024
+ */
+
+
+export const enum Char {
+    empty = "",
+    space = " ",
+    newLine = "\n",
+}
+
+export const enum Language {
+    unknown = "unknown",
+    tabbed = "tabbed",
+}
